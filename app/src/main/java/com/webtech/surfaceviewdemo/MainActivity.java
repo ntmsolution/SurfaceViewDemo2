5a978b5c3e9374b9ffbf67c018dfa5b69214e5bb
ntmsolution <50775414+ntmsolution@users.noreply.github.com>
2019-09-28 10:14:54 +0530

    change done

--- app/src/main/java/com/webtech/surfaceviewdemo/MainActivity.java
+++ app/src/main/java/com/webtech/surfaceviewdemo/MainActivity.java
@@ -2,12 +2,20 @@ package com.webtech.surfaceviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout view1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new MySurface(getApplicationContext()));
+        setContentView(R.layout.activity_main);
+
+        view1 = findViewById(R.id.view1);
+        View v = new MySurface(getApplicationContext());
+
+        view1.addView(v);
     }
 }
